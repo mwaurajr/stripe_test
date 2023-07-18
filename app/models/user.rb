9c3e2e3de5ec6a7cbf1b457b1aa650bb79ad6369
mwaurajr <jamhurifrankiline@gmail.com>
2023-07-18 19:53:20 +0300

    added some methods to the user.rb file e.g  assign_customer_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
+  default_scope { order(created_at: :desc ) }
+  has_many :products
+
+  after_save :assign_customer_id 
+  def assign_customer_id
+    if self.customer_id.blank? 
+      customer = Stripe::Customer.create(email: email)
+      self.update(customer_id: customer.id)
+    end
+  end
+
+  def products_purchased
+    self.products.where.not(paid_at: nil)
+  end
+
+  def products_subscribed_on
+    self.products.where.not(subscribed_at: nil)
+  end
 end
